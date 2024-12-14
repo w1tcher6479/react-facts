b6e02f6171418147e488abef709ce91f05a4c6b0
Anton Kurashkevich <mr.kur.anton@gmail.com>
2024-12-15 01:05:55 +0300

    feat: project complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <nav className="nav">
+                <img className="logo" src="/src/images/react-logo.png" alt="React logo"/>
+                <span className="logo-text">ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <main className="main">
+            <h1 className="title">Fun facts about React</h1>
+            <ul className="facts-list">
+                <li className="facts-list__item">Was first released in 2013</li>
+                <li className="facts-list__item">Was originally created by Jordan Walke</li>
+                <li className="facts-list__item">Has well over 200K stars on GitHub</li>
+                <li className="facts-list__item">Is maintained by Meta</li>
+                <li className="facts-list__item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
